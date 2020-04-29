a2a6295642827f98483d9745ee798adb4be4c08b
RaidenDilan <raiden18@me.com>
2020-04-29 23:38:03 +0100

    Handling Errors

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ const app = express();
 
 app.use('/api/places', placesRoutes); // => /api/places/...
 
+// if 4 arguements are attached to any middleware function, express will recognise this as a special middleware funtion, a error handling middleware function
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+
+  res
+    .status(err.code || 500)
+    .json({ message: err.message || 'An unknown error occurred!' });
+});
+
 app.listen(3000);
--- routes/places/index.js
+++ routes/places/index.js
@@ -21,12 +21,28 @@ router.get('/:pid',(req, res, next) => {
   // console.log('GET request in Places');
   const placeId = req.params.pid; // => { pid: 'p1' }
   const place = DUMMY_PLACES.find(p => p.id === placeId);
+
+  if (!place) {
+    // return res.status(404).json({ message: 'Could not find a place for the provided id.' });
+    const error = new Error('Could not find a place for the provided id.'); // for syncronous middle
+    error.code = 404;
+    throw error; // for syncronous middle
+  }
+
   res.json({ place }); // => { place } => { place: place }
 });
 
 router.get('/user/:uid',(req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find(p => p.creator === userId);
+
+  if (!place) {
+    // return res.status(404).json({ message: 'Could not find a place for the provided user id.' });
+    const error = new Error('Could not find a place for the provided user id.'); // for syncronous middle
+    error.code = 404;
+    return next(error); // for asyncronous middle
+  }
+
   res.json({ place });
 });
 
