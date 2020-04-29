f49314df31e31a3f02a2c39f705f5b4f3b5897f2
RaidenDilan <raiden18@me.com>
2020-04-29 23:15:59 +0100

    Adding Place-Specific Routes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const placesRoutes = require('./routes/places');
 
 const app = express();
-
-app.use(placesRoutes);
 // app.use(bodyParser);
+
+app.use('/api/places', placesRoutes); // => /api/places/...
 
 app.listen(3000);
--- routes/places/index.js
+++ routes/places/index.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-  console.log('GET request in Places');
-  res.json({ message: 'It works!' });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl: 'https://cdn.pixabay.com/photo/2015/12/08/00/40/empire-state-building-1081929_1280.jpg',
+    address: '20 W 34th St, New York, NY 10001, United States',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531
+    },
+    creator: 'u1'
+  }
+];
+
+router.get('/:pid',(req, res, next) => {
+  // console.log('GET request in Places');
+  const placeId = req.params.pid; // => { pid: 'p1' }
+  const place = DUMMY_PLACES.find(place => place.id === placeId);
+  res.json({ place, message: 'It works!' }); // => { place } => { place: place }
 });
 
 module.exports = router;
